1dead01e6787ad4892286f1f71bf4b71f235d8b7
manuelfirman <manuel.firman@gmail.com>
2022-10-17 23:50:57 -0300

    h

--- PORTFOLIOV2/assets/js/main.js
+++ PORTFOLIOV2/assets/js/main.js
@@ -43,3 +43,85 @@
         clickable: true,
       },
     });
+
+    /*===================== ACORDEON SKILLS ========================= */
+    const skillsContent = document.getElementsByClassName('skills__content'),
+                skillsHeader = document.querySelectorAll('.skills__header');
+
+    
+    function toggleSkills(){
+        let itemClass = this.parentNode.className
+
+        for(i = 0; i < skillsContent.length; i++){
+            skillsContent[i].className = "skills__content skills__close";
+        }
+
+        if(itemClass === 'skills__content skills__close'){
+            this.parentNode.className = "skills__content skills__open";
+        }
+    };
+
+    skillsHeader.forEach((el) =>{
+        el.addEventListener('click', toggleSkills)
+    });
+
+/*===================== SCROLL SECTIONS ACTIVE LINK ========================= */
+    const sections = document.querySelectorAll("section[id]");
+
+    function scrollActive() {
+      const scrollY = window.pageYOffset;
+
+      sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+        } else {
+          document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+        }
+      });
+    }
+    window.addEventListener("scroll", scrollActive);
+
+
+    /*===================== CAMBIAR FONDO HEADER ========================= */
+    function scrollHeader(){
+        const nav = document.getElementById('header');
+        if(this.scrollY >= 80) nav.classList.add('scroll-header');
+        else nav.classList.remove('scroll-header');
+    }
+    window.addEventListener('scroll', scrollHeader);
+    
+    /*===================== MOSTRAR SCROLL UP ========================= */
+    function scrollUp(){
+        const scrollUp = document.getElementById('scrollup');
+        if(this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+        else scrollUp.classList.remove('show-scroll');
+    }
+    window.addEventListener('scroll', scrollUp);
+
+        /*===================== TEMA DARK/LIGHT  ========================= */
+    const themeButton = document.getElementById('theme-button');
+    const darkTheme = 'dark-theme';
+    const iconTheme = 'uil-sun'
+
+    const selectedTheme = localStorage.getItem('selected-theme');
+    const selectedIcon = localStorage.getItem('selected-icon');
+
+    const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+    const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+    if(selectedTheme){
+        document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+        themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+    }
+
+    themeButton.addEventListener('click', () =>{
+        document.body.classList.toggle(darkTheme);
+        themeButton.classList.toggle(iconTheme);
+
+        localStorage.setItem('selected-theme', getCurrentTheme())
+        localStorage.setItem('selected-icon', getCurrentIcon())
+    })
